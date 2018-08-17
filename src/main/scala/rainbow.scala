2ae73f4b93fea4ac163d320d3f3ad17139e8e678
Matthew Drescher <knavely@gmail.com>
2018-08-17 16:45:41 -0700

    verify all colors for 6 example

--- src/main/scala/rainbow.scala
+++ src/main/scala/rainbow.scala
@@ -28,8 +28,70 @@ object Rainbow extends App {
     }
   }
 
+  def getRainbowSets(colors: List[Int], n:Int):List[List[Int]] = {
+    val subs = subsets((0 until 2*n).toList).filter(s => s.size <= n/2 && s.size >= 1 && isRainbow(s,colors))
+    subs
+  }
+
   def matFromSet(set: Seq[List[Int]]): List[DenseVector[Int]] = {
     set.map{v => DenseVector(v.toArray)}.toList
+  }
+
+  def bla() = {
+    val example = List(List(0,0,1,1,0),
+      List(0,1,1,1,1), List(1,1,1,0,1), List(1,0,0,0,0), List(1,0,1,0,1),
+      List(0,1,0,1,0), List(1,1,0,0,1), List(1,0,0,0,1), List(0,0,0,1,1),
+      List(0,1,0,1,1), List(0,1,1,1,0), List(1,1,1,0,0))
+
+    def add(li:List[Int], l2:List[Int], l3:List[Int]):List[Int] = {
+      li.zip(l2).zip(l3).map{ case(p,x) => (p._1 + p._2 + x) % 2}
+    }
+
+    def ad(li:List[Int], l2:List[Int]):List[Int] = {
+      li.zip(l2).map{ case(p,x) => (p + x) % 2}
+    }
+
+    val triples = for {
+      x <-example
+      y <- example
+      z <- example
+      if(!((x == y) || (x == z) || (y == z))) 
+    } yield (x,y,z)
+
+    val doubles = for {
+      x <-example
+      y <- example
+      if(!(x == y))
+    } yield (x,y)
+
+    println(triples.map{case((a,b,c)) => add(a,b,c)})
+    doubles.foreach{case((a,b)) =>
+      if(ad(a,b) == List(0,0,0,0,0))
+        println(List(a,b))}
+    triples.forall{ case((a,b,c)) => add(a,b,c) != List(0,0,0,0,0)} &&
+    doubles.forall{case((a,b)) => ad(a,b) != List(0,0,0,0,0)}
+  }
+
+  def test() = {
+    val n = 6
+    val genColors = Gen.containerOfN[List,Int](2*n, arbitrary[Int]).map{l => color(n)}
+    val example = List(List(0,0,1,1,0),
+      List(0,1,1,1,1), List(1,1,1,0,1), List(1,0,0,0,0), List(1,0,1,0,1),
+      List(0,1,0,1,0), List(1,1,0,0,1), List(1,0,0,0,1), List(0,0,0,1,1),
+            List(0,1,0,1,1), List(0,1,1,1,0), List(1,1,1,0,0))
+    val mat = matFromSet(example)
+
+    val prop = forAll(genColors) {
+      colors:List[Int] =>
+      val sets = getRainbowSets(colors,6)
+      sets.forall{
+        s =>
+        val sum = s.foldLeft(DenseVector.zeros[Int]((n-1))){(acc,i) => (acc + mat(i)) % 2}
+        sum != DenseVector.zeros[Int]((n-1))
+      }
+    }
+    val param = org.scalacheck.Test.Parameters.defaultVerbose
+    prop.check(param)
   }
 
   def rain(n:Int) = {
@@ -68,6 +130,7 @@ object Rainbow extends App {
   }
 
   val param = org.scalacheck.Test.Parameters.defaultVerbose.withMaxDiscardRatio(1000000.0f).withMinSuccessfulTests(1)
-  rain(8).check(param)
+  //rain(8).check(param)
+  test()
 }
 
